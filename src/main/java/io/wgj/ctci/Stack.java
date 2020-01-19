cd6dc83cc0daf94827e214ba2a7c26f33c9aef00
Weston Johnson <wes@futureprefect.com>
2020-01-19 00:57:48 -0700

    Use generics, remove unneeded tests, add dynamic array
    Also adds a class description, quality of life methods, and associated tests

--- src/main/java/io/wgj/ctci/Stack.java
+++ src/main/java/io/wgj/ctci/Stack.java
@@ -1,21 +1,80 @@
 package io.wgj.ctci;
 
-public class Stack {
-    int[] stack;
+/**
+ * Stack is a stack data structure backed by a dynamic array. Stack can be
+ * initialized with a starting array size. It's written to develop familiarity
+ * with the stack data structure, and should otherwise not be used.
+ * Maximum allowed array is 1024. If you set a starting capacity larger than
+ * this, the maximum allowed array size is used as the starting capacity. An
+ * UnsupportedOperationException will be thrown when more the maximum allowed
+ * amount of elements are added.
+ */
+public class Stack<E> {
+    static final int MAX_ARRAY_SIZE = 1024;
+    static final int DEFAULT_CAPACITY = 10;
+    Object[] stack;
+    int size;
     int position;
 
     public Stack() {
-        stack = new int[10];
+        this(DEFAULT_CAPACITY);
+    }
+
+    public Stack(int size) {
+        stack = new Object[size];
+        if (size > MAX_ARRAY_SIZE) {
+            size = MAX_ARRAY_SIZE;
+        }
+        this.size = size;
         position = 0;
     }
 
-    public void push(int i) {
-        stack[position] = i;
+    public void push(E e) {
+        if (size == position) {
+            this.grow();
+        }
+        stack[position] = e;
         position++;
     }
 
-    public int pop() {
+    public E pop() {
         position--;
-        return stack[position];
+        @SuppressWarnings("unchecked")
+        E e = (E) stack[position];
+        return e;
+    }
+
+    private void grow() {
+        int newSize = size << 2;
+        if (newSize > MAX_ARRAY_SIZE) {
+            int leftOver = MAX_ARRAY_SIZE - size;
+            // Squeeze out the remaining space we'd lose.
+            if (leftOver > 0) {
+                newSize = leftOver + size;
+            } else {
+                throw new UnsupportedOperationException();
+            }
+        }
+        Object[] newStack = new Object[newSize];
+        size = newSize;
+        stack = newStack;
+    }
+
+    public E peek() {
+        @SuppressWarnings("unchecked")
+        E e = (E) stack[position - 1];
+        return e;
+    }
+
+    public int size() {
+        return position;
+    }
+
+    public int capacity() {
+        return size;
+    }
+
+    public int maxSize() {
+        return MAX_ARRAY_SIZE;
     }
 }
--- src/test/java/io/wgj/ctci/StackTest.java
+++ src/test/java/io/wgj/ctci/StackTest.java
@@ -6,10 +6,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Random;
-import java.util.stream.IntStream;
 
 public class StackTest {
     @Rule
@@ -17,62 +13,110 @@ public class StackTest {
 
     @Test
     public void testStackOneInt() {
-        Stack s = new Stack();
+        Stack<Integer> s = new Stack<>();
         s.push(1);
         int r = s.pop();
         assertEquals(1, r);
     }
 
     @Test
+    public void testStackOneString() {
+        Stack<String> s = new Stack<>();
+        s.push("one");
+        String r = s.pop();
+        assertEquals("one", r);
+    }
+
+    @Test
+    public void testPushPopsIntermixed() {
+        Stack<Integer> s = new Stack<>();
+        s.push(0);
+        assertEquals(0, (int) s.pop());
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        assertEquals(3, (int) s.pop());
+        assertEquals(2, (int) s.pop());
+        s.push(4);
+        assertEquals(4, (int) s.pop());
+        assertEquals(1, (int) s.pop());
+    }
+
+    @Test
     public void testStackMultipleInts() {
-        ArrayList<Integer> expected = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            expected.add(i);
-        }
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
 
-        Stack s = new Stack();
-        for (int e : expected) {
-            s.push(e);
-        }
-
-        Collections.reverse(expected);
-        for (int e : expected) {
-            assertEquals(e, s.pop());
-        }
+        assertEquals(3, (int) s.pop());
+        assertEquals(2, (int) s.pop());
+        assertEquals(1, (int) s.pop());
     }
 
     @Test
-    public void testStackRandomInts() {
-        Random r = new Random();
+    public void testGrowth() {
+        Stack<Integer> s = new Stack<>(1);
+        int startingCapacity = s.capacity();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        int newCapacity = s.capacity();
+        assertTrue(startingCapacity < newCapacity);
 
-        ArrayList<Integer> expected = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            expected.add(r.nextInt());
-        }
-
-        Stack s = new Stack();
-        for (int e : expected) {
-            s.push(e);
-        }
-
-        Collections.reverse(expected);
-        for (int e : expected) {
-            assertEquals(e, s.pop());
-        }
     }
 
     @Test
-    public void testStackOverFlow() {
-        Stack s = new Stack();
-        for (int i = 0; i < 10; i++) {
-            s.push(i);
+    public void testGrowthWithLeftOver() {
+        int cap = 257; // 257 << 2 is 1028, and trigger a "left over".
+        Stack<Integer> s = new Stack<>(cap);
+        assertEquals(cap, s.capacity());
+        for (int i = 0; i < cap + 1; i++) {
+            s.push(0);
         }
-        thrown.expect(ArrayIndexOutOfBoundsException.class);
-        s.push(11);
+        // Our new capacity will use what's left over from s.grow().
+        assertEquals(s.maxSize(), s.capacity());
+    }
+
+    @Test
+    public void testMaxArraySize() {
+        Stack<Integer> s = new Stack<>(1024); // Stack.MAX_ARRAY_SIZE
+        for (int i = 0; i < s.maxSize(); i++) {
+            s.push(0);
+        }
+        thrown.expect(UnsupportedOperationException.class);
+        s.push(0);
+    }
+
+    @Test
+    public void testTooLargeStartingArray() {
+        Stack<Integer> s = new Stack<>(2048);
+        assertEquals(1024, s.maxSize());
     }
 
     @Test(expected = ArrayIndexOutOfBoundsException.class)
     public void testStackUnderFlow() {
         new Stack().pop();
     }
+
+    @Test
+    public void TestPeek() {
+        Stack<Integer> s = new Stack<>();
+        s.push(0);
+        assertEquals(0, (int) s.peek());
+    }
+
+    @Test
+    public void TestSize() {
+        Stack<Integer> s = new Stack<>();
+        s.push(0);
+        assertEquals(1, s.size());
+    }
+
+    @Test
+    public void TestSizeEmptyStack() {
+        Stack<Integer> s = new Stack<>();
+        assertEquals(0, s.size());
+    }
+
 }
