5daa0be14f1d62436cbff1f1644ecb973ee6cc5b
Weston Johnson <wes@futureprefect.com>
2020-01-26 17:09:12 -0700

    wip

--- src/main/java/io/wgj/ctci/Queue.java
+++ src/main/java/io/wgj/ctci/Queue.java
@@ -1,43 +1,43 @@
 package io.wgj.ctci;
 
 public class Queue<E> {
+
   Object[] queue;
   int head;
   int tail;
-  int length;
-  int capacity;
-  static final int STARTING_CAPACITY = 10;
+  static final int STARTING_CAPACITY = 1;
+  int size;
 
   public Queue() {
     queue = new Object[STARTING_CAPACITY];
     head = 0;
     tail = 0;
-    length = 0;
-    capacity = STARTING_CAPACITY;
+    size = 0;
   }
 
   public void add(E e) {
-    if (length == capacity) {
+    // TODO(wgj): Handle wrap around.
+    if (getSize() == queue.length) {
       grow();
     }
-    queue[tail] = e;
-    tail++;
-    length++;
+    queue[nextTail()] = e;
+    size++;
+    tail = nextTail();
   }
 
   public E remove() {
-    if (length == 0) {
-      throw new ArrayIndexOutOfBoundsException();
+    if (size == 0) {
+      throw new IndexOutOfBoundsException();
     }
     @SuppressWarnings("unchecked")
     E e = (E) queue[head];
     head++;
-    length--;
+    size--;
     return e;
   }
 
   public E peek() {
-    if (length == 0) {
+    if (size == 0) {
       return null;
     }
     @SuppressWarnings("unchecked")
@@ -46,25 +46,39 @@ public class Queue<E> {
   }
 
   private void grow() {
-    int newCap = capacity * 2;
-    Object[] newQueue = new Object[newCap];
+    int newLength = queue.length * 2;
+    Object[] newQueue = new Object[newLength];
     // Loop from head to cap
-    int copyPointer = 0;
-    for (int i = head; i < capacity - 1; i++) {
+    int copyIndex = 0;
+    for (int i = head; i < queue.length - 1; i++) {
       newQueue[i] = queue[i];
-      copyPointer++;
+      copyIndex++;
     }
     // Loop from 0 to tail
-    for (int i = 0; i <= tail; i++) {
-      newQueue[copyPointer + i] = queue[i];
+    for (int i = 0; i < tail; i++) {
+      newQueue[copyIndex + i] = queue[i];
     }
     queue = newQueue;
-    capacity = newCap;
     head = 0;
-    tail = length;
+    tail = size;
   }
 
-  public int getLength() {
-    return length;
+  public int getSize() {
+    return size;
+  }
+
+  private int nextHead() {
+    return -1;
+  }
+
+  private int nextTail() {
+    // Wrap around since we're at the end of `queue`.
+    if (tail > 0 && tail == queue.length - 1) {
+      return 0;
+    } else if (head == tail) {
+      return tail;
+    } else {
+      return tail++;
+    }
   }
 }
--- src/test/java/io/wgj/ctci/QueueTest.java
+++ src/test/java/io/wgj/ctci/QueueTest.java
@@ -20,29 +20,68 @@ public class QueueTest {
   @Test
   public void add_newQueue() {
     Queue<Integer> q = new Queue<>();
-    q.add(0);
+    q.add(1);
   }
 
   @Test
   public void remove_newQueue() {
     Queue<Integer> q = new Queue<>();
-    exception.expect(ArrayIndexOutOfBoundsException.class);
+    exception.expect(IndexOutOfBoundsException.class);
     q.remove();
   }
 
   @Test
-  public void add_remove() {
+  public void add_remove_intermixed() {
     Queue<Integer> q = new Queue<>();
     q.add(1);
     int a = q.remove();
     assertEquals(1, a);
-    assertEquals(0, q.getLength());
+    assertEquals(0, q.getSize());
     q.add(2);
     q.add(3);
     a = q.remove();
-    assertEquals(2, 2);
-    assertEquals(1, q.getLength());
+    assertEquals(2, a);
+    assertEquals(1, q.getSize());
   }
+
+  @Test
+  public void add_wrapAround() {
+    Queue<Integer> q = new Queue<>();
+    q.add(1);
+    assertEquals(1, q.getSize());
+    q.add(2);
+    assertEquals(2, q.getSize());
+    q.add(3);
+    assertEquals(3, q.getSize());
+    q.add(4);
+    assertEquals(4, q.getSize());
+    q.add(5);
+    assertEquals(5, q.getSize());
+    q.add(6);
+    assertEquals(6, q.getSize());
+    q.add(7);
+    assertEquals(7, q.getSize());
+    q.add(8);
+    assertEquals(8, q.getSize());
+    q.add(9);
+    assertEquals(9, q.getSize());
+    q.add(10);
+    assertEquals(10, q.getSize());
+
+    assertEquals(1, (int) q.remove());
+    assertEquals(2, (int) q.remove());
+    assertEquals(3, (int) q.remove());
+    assertEquals(4, (int) q.remove());
+    assertEquals(5, (int) q.remove());
+    assertEquals(6, (int) q.remove());
+    assertEquals(7, (int) q.remove());
+    assertEquals(8, (int) q.remove());
+    assertEquals(9, (int) q.remove());
+    assertEquals(10, (int) q.remove());
+
+    assertEquals(0, q.getSize());
+  }
+
 
   @Test
   public void peek_newQueue() {
