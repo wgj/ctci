ac62388943eb1b5008f09f53a21067195c11312e
Weston Johnson <wes@futureprefect.com>
2022-07-29 14:26:32 -0600

    Refactor Stack to use io.wgj.ctci.ArrayList
    Remove unused or unneeded methods and fields.

--- src/main/java/io/wgj/ctci/Stack.java
+++ src/main/java/io/wgj/ctci/Stack.java
@@ -1,65 +1,31 @@
 package io.wgj.ctci;
 
-import static java.util.Arrays.copyOf;
-
 /**
- * Stack is a stack data structure backed by a dynamic array. Stack can be
- * initialized with a starting array size. It's written to develop familiarity
- * with the stack data structure, and should otherwise not be used.
- * Maximum allowed array is 1024. If you set a starting capacity larger than
- * this, the maximum allowed array size is used as the starting capacity. An
- * UnsupportedOperationException will be thrown when more the maximum allowed
- * amount of elements are added.
+ * Stack is a stack data structure backed by a dynamic array. It's written to
+ * develop familiarity with the stack data structure.
+ *
  */
 public class Stack<E> {
-    static final int DEFAULT_CAPACITY = 2;
-    Object[] stack;
-    int capacity;
-    int size;
+    ArrayList<E> al;
 
     public Stack() {
-        this(DEFAULT_CAPACITY);
-    }
-
-    public Stack(int capacity) {
-        stack = new Object[capacity];
-        this.capacity = capacity;
-        size = 0;
+        al = new ArrayList<>();
     }
 
     public void push(E e) {
-        if (capacity == size) {
-            this.grow();
-        }
-        stack[size] = e;
-        size++;
+        al.add(e);
     }
 
     public E pop() {
-        size--;
-        @SuppressWarnings("unchecked")
-        E e = (E) stack[size];
-        return e;
-    }
-
-    private void grow() {
-        int newCap = capacity * 2;
-        Object[] newStack = copyOf(stack, newCap);
-        capacity = newCap;
-        stack = newStack;
+        // This shouldn't been costly, since we're always removing from the top of the stack.
+        return al.remove(al.size()-1);
     }
 
     public E peek() {
-        @SuppressWarnings("unchecked")
-        E e = (E) stack[size - 1];
-        return e;
+        return al.get(al.size()-1);
     }
 
     public int size() {
-        return size;
-    }
-
-    public int capacity() {
-        return capacity;
+        return al.size();
     }
 }
